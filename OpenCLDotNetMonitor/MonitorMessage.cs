fe43ffc2ac0c89859bf291a1798bd28c00c8e55a
Davide Morelli <info@davidemorelli.it>
2012-06-03 01:07:00 +0200

    created c sharp monitor with helper class

---
+++ OpenCLDotNetMonitor/MonitorMessage.cs
@@ -0,0 +1,168 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace OpenCLDotNetMonitor
+{
+    enum OpCodes
+    {
+        CREATE,
+        OK,
+        ENUMERATE_COUNTERS,
+        PERF_INIT,
+        RELEASE,
+        GET_COUNTERS,
+        END
+    }
+
+    public class MonitorMessage
+    {
+        /// <summary>
+        /// the message type. 
+        /// </summary>
+        public OpCodes OpCode { get; set; }
+        /// <summary>
+        /// the message type as row integer value
+        /// </summary>
+        public int OpCode_Raw { get; set; }
+        /// <summary>
+        /// 0 if successful, non zero otherwise
+        /// </summary>
+        public int As { get; set; }
+        /// <summary>
+        /// error code if As != 0
+        /// </summary>
+        public int Aps { get; set; }
+        /// <summary>
+        /// the content of the message
+        /// </summary>
+        public string[] Body { get; set; }
+
+        private MonitorMessage()
+        {
+        }
+        
+        /// <summary>
+        /// Parse a MonitorMessage out of a string received on the pipe
+        /// </summary>
+        /// <param name="rawString">the string received on the pipe</param>
+        /// <returns></returns>
+        public static MonitorMessage ParseFromString(string rawString)
+        {
+            MonitorMessage m = new MonitorMessage();
+            string[] tags = rawString.Split(new string[] { " " }, StringSplitOptions.RemoveEmptyEntries);
+            if (tags.Length < 3)
+                throw new ArgumentException("raw string does not contain the minimum number of needed arguments");
+            int opcode = -1;
+            if (int.TryParse(tags[0], out opcode))
+            {
+                m.OpCode_Raw = opcode;
+                m.OpCode = (OpCodes)opcode;
+            }
+            else
+            {
+                throw new ArgumentException("the first argument is not a valid value, not an integer value", "Operation type");
+            }
+            int err = -1;
+            if (int.TryParse(tags[1], out err))
+            {
+                m.As = err;
+            }
+            else
+            {
+                throw new ArgumentException("the second argument is not a valid value, not an integer value", "As");
+            }
+            int errDesc = 0;
+            if (int.TryParse(tags[2], out errDesc))
+            {
+                m.Aps = errDesc;
+            }
+            else
+            {
+                throw new ArgumentException("the third argument is not a valid value, not an integer value", "Aps");
+            }
+            if (tags.Length > 3)
+            {
+                m.Body = tags.Skip(3).ToArray();
+            }
+            else
+            {
+                m.Body = new string[] { };
+            }
+            return m;            
+        }
+
+        /// <summary>
+        /// create an instance of a MonitorMessage with a list of strings as the message body
+        /// </summary>
+        /// <param name="_op">the operation type</param>
+        /// <param name="_as">0 if operation was succesfull, non zero otherwise</param>
+        /// <param name="_aps">error code, 0 if not error</param>
+        /// <param name="_body">body of the message</param>
+        public MonitorMessage(OpCodes _op, int _as, int _aps, string[] _body)
+        {
+            MonitorMessage m = new MonitorMessage();
+            m.Body = _body;
+            m.As = _as;
+            m.Aps = _aps;
+            m.OpCode = _op;
+            m.OpCode_Raw = (int) _op;
+        }
+
+        /// <summary>
+        /// create an instance of a MonitorMessage with a list of floats as the message body
+        /// </summary>
+        /// <param name="_op">the operation type</param>
+        /// <param name="_as">0 if operation was succesfull, non zero otherwise</param>
+        /// <param name="_aps">error code, 0 if not error</param>
+        /// <param name="_body">body of the message</param>
+        public MonitorMessage(OpCodes _op, int _as, int _aps, float[] _body)
+        {
+            MonitorMessage m = new MonitorMessage();
+
+            m.Body = _body.Select(x => x.ToString()).ToArray() ;
+            m.As = _as;
+            m.Aps = _aps;
+            m.OpCode = _op;
+            m.OpCode_Raw = (int)_op;
+        }
+
+        /// <summary>
+        /// create an instance of a MonitorMessage with a single string as message body
+        /// </summary>
+        /// <param name="_op">the operation type</param>
+        /// <param name="_as">0 if operation was succesfull, non zero otherwise</param>
+        /// <param name="_aps">error code, 0 if not error</param>
+        /// <param name="_body">body of the message</param>
+        public MonitorMessage(OpCodes _op, int _as, int _aps, string _body)
+        {
+            MonitorMessage m = new MonitorMessage();
+
+            m.Body = new string[] { _body };
+            m.As = _as;
+            m.Aps = _aps;
+            m.OpCode = _op;
+            m.OpCode_Raw = (int)_op;
+        }
+
+        /// <summary>
+        /// get a string usable on the pipe from this MonitorMessage
+        /// </summary>
+        /// <returns></returns>
+        public override string ToString()
+        {
+            StringBuilder sb = new StringBuilder();
+            sb.AppendFormat("{0} {1} {2} ", OpCode_Raw, As, Aps);
+            foreach (string b in Body)
+            {
+                sb.AppendFormat("{0} ", b);
+            }
+            // remove last space
+            if (Body.Length > 0)
+                sb.Remove(sb.Length - 1, 1);
+            return sb.ToString();
+        }
+
+    }
+}
---
+++ OpenCLDotNetMonitor/Server.cs
@@ -0,0 +1,136 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.IO.Pipes;
+using System.IO;
+using System.Threading;
+
+namespace OpenCLDotNetMonitor
+{
+    public class OpenCLMonitorServer
+    {
+        private static bool shouldStop = false;
+        private static int serverInstances = 1;
+        NamedPipeServerStream pipeServer;
+
+        public void StartServer()
+        {
+            //Create a unique pipe name.  
+            string pipeName = "openclmonitor_pipe";
+            System.Security.Principal.SecurityIdentifier sid = new System.Security.Principal.SecurityIdentifier(System.Security.Principal.WellKnownSidType.WorldSid, null);
+            PipeAccessRule pac = new PipeAccessRule(sid, PipeAccessRights.ReadWrite, System.Security.AccessControl.AccessControlType.Allow);
+            PipeSecurity ps = new PipeSecurity();
+            ps.AddAccessRule(pac);
+            sid = null; 
+            pipeServer = new NamedPipeServerStream(pipeName,
+                    PipeDirection.InOut,
+                    serverInstances,
+                    PipeTransmissionMode.Byte,
+                    PipeOptions.Asynchronous,
+                    1,
+                    1,
+                    ps);
+            AsyncCallback myCallback = new AsyncCallback(AsyncPipeCallback);
+            pipeServer.BeginWaitForConnection(myCallback, null);
+        }
+
+        public void StopServer()
+        {
+            if (pipeServer.IsConnected)
+                pipeServer.Disconnect();
+            pipeServer.Dispose();
+        }
+
+        /// <summary>
+        /// creates an unique pipe name
+        /// </summary>
+        /// <returns></returns>
+        private string CreatePipeName()
+        {
+            return System.Guid.NewGuid().ToString("N");
+        }
+
+        private void AsyncPipeCallback(IAsyncResult Result)
+        {
+            Console.WriteLine("AsyncPipeCallback Entered.");
+            try
+            {
+                pipeServer.EndWaitForConnection(Result);
+                StreamString ss = new StreamString(pipeServer);
+                MonitorMessage messageIN = MonitorMessage.ParseFromString(ss.ReadString());
+                if (messageIN.As == 0)
+                {
+                    // TODO: check return code is zero
+                    // create pipe name
+                    string newPipeName = CreatePipeName();
+                    // create thread
+                    ThreadPool.QueueUserWorkItem(new WaitCallback(HandleQueueThread), newPipeName);
+                    // TODO: wait for the new pipe server to be ready...
+                    // LOCK ?
+
+                    // send pipe name
+                    MonitorMessage messageOUT = new MonitorMessage(OpCodes.OK, 0, 0, newPipeName);
+                    ss.WriteString(messageOUT.ToString());
+                }
+                else
+                {
+                    // received a message with error code
+                    Console.WriteLine("return code {0}, error code {1}", messageIN.As, messageIN.Aps);
+                }
+            }
+            catch (OperationCanceledException)
+            {
+                Console.WriteLine("Oops, exiting the thread that started the BeginWaitForConnection() on this pipe has cancelled BeginWaitForConnection().");
+                Console.WriteLine("WHY???");
+            }
+        }
+
+        static void HandleQueueThread(Object stateInfo)
+        {
+            // the pipe here will be handled synchronously
+            string pipename = (string)stateInfo;
+            System.Security.Principal.SecurityIdentifier sid = new System.Security.Principal.SecurityIdentifier(System.Security.Principal.WellKnownSidType.WorldSid, null);
+            PipeAccessRule pac = new PipeAccessRule(sid, PipeAccessRights.ReadWrite, System.Security.AccessControl.AccessControlType.Allow);
+            PipeSecurity ps = new PipeSecurity();
+            ps.AddAccessRule(pac);
+            sid = null;
+
+            using (NamedPipeServerStream queuePipe = new NamedPipeServerStream(pipename,
+                    PipeDirection.InOut,
+                    serverInstances,
+                    PipeTransmissionMode.Byte,
+                    PipeOptions.None,
+                    1,
+                    1,
+                    ps))
+            {
+                queuePipe.WaitForConnection();
+                try
+                {
+                    // Read the request from the client. Once the client has
+                    // written to the pipe its security token will be available.
+
+                    StreamString ss = new StreamString(queuePipe);
+
+                    // Verify our identity to the connected client using a
+                    // string that the client anticipates.
+
+                    string input = ss.ReadString();
+
+                }
+                // Catch the IOException that is raised if the pipe is broken
+                // or disconnected.
+                catch (IOException e)
+                {
+                    Console.WriteLine("ERROR: {0}", e.Message);
+                }
+                queuePipe.Close();
+            }
+        }
+ 
+    }
+
+
+
+}

e287e5c528224f27ae4af1358d70c4282d005c58
Davide Morelli <info@davidemorelli.it>
2012-06-04 10:00:07 +0200

    adding tester project

---
+++ OpenCLMonitorTester/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using OpenCLDotNetMonitor;
+
+namespace OpenCLMonitorTester
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            OpenCLMonitorServer server = new OpenCLMonitorServer();
+            server.StartServer(new OpenCLMonitorServer.SelectCounter(SelectCounters), new OpenCLMonitorServer.ReceivedValues(ReceivedValues));
+            Console.WriteLine("waiting for data, press enter to stop");
+            Console.ReadLine();
+            server.StopServer();
+        }
+
+        static void ReceivedValues(string device, float[] values)
+        {
+            StringBuilder sb = new StringBuilder();
+            sb.AppendFormat("{0}:", device);
+            foreach (float v in values)
+            {
+                sb.AppendFormat("{0},", v);
+            }
+            Console.WriteLine(sb.ToString());
+        }
+
+        static string[] SelectCounters(string device, string[] counters)
+        {
+            return new string[] { counters[0]};
+        }
+    }
+}
